676eeabee41dfa142672a311f03d72a5d8579e9e
TheAlpha_07 <alfredmeuleman@icloud.com>
2023-05-23 16:32:43 +0200

    .

--- src/main/java/origins/evolved/originsevolved/OriginsEvolved.java
+++ src/main/java/origins/evolved/originsevolved/OriginsEvolved.java
@@ -1,2 +1,15 @@
-public class OriginsEvolved {
+package origins.evolved.originsevolved;
+
+import net.fabricmc.api.ModInitializer;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class OriginsEvolved implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogManager().getLogger("originsevolved");
+    public static final String MOD_ID = "originsevolved";
+
+    @Override
+    public void onInitialize() {
+
+    }
 }
---
+++ src/main/java/origins/evolved/originsevolved/mixin/OriginsEvolvedMixin.java
@@ -0,0 +1,17 @@
+package origins.evolved.originsevolved.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import origins.evolved.originsevolved.OriginsEvolved;
+
+
+@Mixin(TitleScreen.class)
+public class OriginsEvolvedMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info)    {
+        OriginsEvolved.LOGGER.info("Loading Origins Evolved");
+    }
+}
