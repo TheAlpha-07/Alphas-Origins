d7fa1143414fc2f4337b08ca88616f7537f85a0e
TheAlpha_07 <alfredmeuleman@icloud.com>
2023-06-22 16:33:32 +0200

    .

--- src/main/java/origins/evolved/originsevolved/OriginsEvolvedClient.java
+++ src/main/java/origins/evolved/originsevolved/OriginsEvolvedClient.java
@@ -1,7 +1,10 @@
 package origins.evolved.originsevolved;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class OriginsEvolvedClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
