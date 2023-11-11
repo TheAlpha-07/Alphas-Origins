f1455467510ca6e99420c40d0884114d2ce4e4c0
alfre <alfredmeuleman@icloud.com>
2023-11-11 18:20:38 +0100

    Additions + changes: - Addition about impact

--- src/main/java/com/thealpha07/alphasorigins/origin/Impact.java
+++ src/main/java/com/thealpha07/alphasorigins/origin/Impact.java
@@ -1,42 +1,53 @@
-/*
-package com.thealpha07.alphasorigins.origin;
 
+/*package com.thealpha07.alphasorigins.origin;
+
+import io.github.apace100.origins.Origins;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 
 public enum Impact {
 
-    MYSTERIOUS(4, "mysterious", Formatting.DARK_PURPLE),
-    ALPHA(5, "alpha", Formatting.BLACK);
+	NONE(0, "none", Formatting.GRAY, Origins.identifier("choose_origin/impact/none")),
+	LOW(1, "low", Formatting.GREEN, Origins.identifier("choose_origin/impact/low")),
+	MEDIUM(2, "medium", Formatting.YELLOW, Origins.identifier("choose_origin/impact/medium")),
+	HIGH(3, "high", Formatting.RED, Origins.identifier("choose_origin/impact/high")),
+	MYSTERY(4, "mystery", Formatting.BLUE, Origins.identifier("choose_origin/impact/mystery"));
 
-    private int impactValue;
-    private String translationKey;
-    private Formatting textStyle;
+	private final int impactValue;
+	private final String translationKey;
+	private final Formatting textStyle;
+	private final Identifier spriteId;
 
-    private Impact (int impactValue, String translationKey, Formatting textStyle) {
-        this.translationKey = "origins.gui.impact." + translationKey;
-        this.impactValue = impactValue;
-        this.textStyle = textStyle;
-    }
+	private Impact(int impactValue, String translationKey, Formatting textStyle, Identifier spriteId) {
+		this.translationKey = "origins.gui.impact." + translationKey;
+		this.impactValue = impactValue;
+		this.textStyle = textStyle;
+		this.spriteId = spriteId;
+	}
 
-    public int getImpactValue() {
-        return impactValue;
-    }
+	public Identifier getSpriteId() {
+		return spriteId;
+	}
 
-    public String getTranslationKey() {
-        return translationKey;
-    }
+	public int getImpactValue() {
+		return impactValue;
+	}
 
-    public Formatting getTextStyle() {
-        return textStyle;
-    }
+	public String getTranslationKey() {
+		return translationKey;
+	}
 
-    public MutableText getTextComponent() {
-        return Text.translatable(getTranslationKey()).formatted(getTextStyle());
-    }
+	public Formatting getTextStyle() {
+		return textStyle;
+	}
 
-    public static Impact getByValue(int impactValue) {
-        return Impact.values()[impactValue];
-    }
+	public MutableText getTextComponent() {
+		return Text.translatable(getTranslationKey()).formatted(getTextStyle());
+	}
+
+	public static Impact getByValue(int impactValue) {
+		return Impact.values()[impactValue];
+	}
 }*/
